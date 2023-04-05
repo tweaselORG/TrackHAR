33337daa35be049eea061c396b5ef3ab48ff42be
Benjamin Altpeter <hi@bn.al>
2023-04-05 11:32:01 +0200

    finish initial implementation

--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,21 @@
-TrackHAR
+trackhar
 
-# TrackHAR
+# trackhar
+
+## Table of contents
+
+### Type Aliases
+
+- [JsonPath](README.md#jsonpath)
+
+## Type Aliases
+
+### JsonPath
+
+Ƭ **JsonPath**: `string`
+
+A JSONPath expression to be parsed by https://github.com/JSONPath-Plus/JSONPath.
+
+#### Defined in
+
+[index.ts:13](https://github.com/tweaselORG/TrackHAR/blob/main/src/index.ts#L13)
--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,13 @@ import type { Request } from './common/request';
 import { unhar } from './common/request';
 import type { ArrayOrSingle } from './common/type-utils';
 
-import h from '../drums.tmp.json';
-
 const allAdapters = getAllAdapters();
 
 /** A JSONPath expression to be parsed by https://github.com/JSONPath-Plus/JSONPath. */
 export type JsonPath = string;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type TrackingDataValue = any;
 
 export type Context = 'header' | 'path' | 'query' | 'body';
 
@@ -69,6 +70,11 @@ export type DecodingStep = (
     | { function: 'getProperty'; options: { path: JsonPath } }
 ) &
     (({ input: Path } | { mapInput: Path }) & { output: Identifier });
+export type DataPath = {
+    context: Context;
+    path: JsonPath;
+    reasoning: 'obvious property name' | 'obvious observed values' | `https://${string}` | `http://${string}`;
+};
 export type Adapter = {
     slug: string;
     tracker: Tracker;
@@ -77,25 +83,11 @@ export type Adapter = {
     match?: (r: Request) => boolean | undefined;
 
     decodingSteps: DecodingStep[];
-    containedDataPaths: Partial<
-        Record<
-            Property,
-            ArrayOrSingle<{
-                context: Context;
-                path: JsonPath;
-                reasoning:
-                    | 'obvious property name'
-                    | 'obvious observed values'
-                    | `https://${string}`
-                    | `http://${string}`;
-            }>
-        >
-    >;
+    containedDataPaths: Partial<Record<Property, ArrayOrSingle<DataPath>>>;
 };
 
 const decodeRequest = (r: Request, decodingSteps: DecodingStep[]) => {
     const [path, query] = r.path.split('?');
-    console.log(r.path, path, query);
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const vars: Record<Variable, any> & { res: Partial<Record<Context, any>> } = {
@@ -126,20 +118,22 @@ const decodeRequest = (r: Request, decodingSteps: DecodingStep[]) => {
 
     for (const step of decodingSteps) {
         if ('mapInput' in step) {
-            const result = get(step.mapInput)
+            const mapInput = get(step.mapInput);
+            if (!Array.isArray(mapInput)) throw new Error('mapInput must be an array.');
+            const result = mapInput
                 .filter((i) => i !== undefined && i !== null)
-                .map((i) => decodeFunctions[step.function](i, step.options));
+                .map((i) => decodeFunctions[step.function](i, (step as { options: unknown }).options));
             if (result) set(step.output, result);
             continue;
         }
+
         const input = get(step.input);
         if (!input) continue;
 
-        const result = decodeFunctions[step.function](input, step.options);
+        const result = decodeFunctions[step.function](input, (step as { options: unknown }).options);
         if (result) set(step.output, result);
     }
 
-    console.log(vars);
     return vars.res;
 };
 
@@ -157,12 +151,43 @@ const processRequest = async (r: Request) => {
     if (!adapter) return undefined;
 
     const decodedRequest = decodeRequest(r, adapter.decodingSteps);
-    console.log(decodedRequest);
+
+    const flattenedPaths = Object.entries(adapter.containedDataPaths)
+        .map(([property, paths]) => (Array.isArray(paths) ? paths : [paths]).map((p) => [property, p] as const))
+        .flat();
+    return flattenedPaths
+        .map(([property, path]) =>
+            JSONPath<TrackingDataValue[]>({ path: path.path, json: decodedRequest[path.context], wrap: true })
+                .map((v) => ({
+                    adapter: `${adapter.tracker.slug}/${adapter.slug}`,
+                    property,
+                    ...path,
+                    value: v,
+                }))
+                .filter((v) => v.value !== undefined && v.value !== null && v.value.trim() !== '')
+        )
+        .flat();
 };
 
-export type Result = Record<string, { source: Context; value: string }>;
-export const process = (har: Har): Result => unhar(har).map(processRequest);
+export type AnnotatedResult = ({ adapter: string; property: string; value: TrackingDataValue } & DataPath)[];
+export type Result = Record<string, TrackingDataValue[]>;
+export const process = async <ValuesOnly extends boolean = false>(
+    har: Har,
+    options?: { valuesOnly?: ValuesOnly }
+): Promise<ValuesOnly extends true ? (Result | undefined)[] : (AnnotatedResult | undefined)[]> => {
+    const res = await Promise.all(unhar(har).map(processRequest));
 
-(async () => {
-    await process(h);
-})();
+    const ret = options?.valuesOnly
+        ? res.map((req) =>
+              req?.reduce<Result>(
+                  (acc, cur) => ({
+                      ...acc,
+                      [cur.property]: acc[cur.property]?.concat(cur.value) || [cur.value],
+                  }),
+                  {}
+              )
+          )
+        : res;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return ret as any;
+};
