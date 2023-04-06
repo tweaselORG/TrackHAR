eb99b9eb06aeec12de482dfce4eb169f6eae321a
Lorenz Sieben <me@lorenz-sieben.com>
2023-04-06 12:12:52 +0200

    Export adapters in the module (#4)

--- src/common/adapters.ts
+++ src/common/adapters.ts
@@ -1,21 +1,5 @@
-import { getDirname } from 'cross-dirname';
-import { readdir } from 'fs/promises';
-import { basename, join } from 'path';
-import type { Adapter } from '../index';
+import { adapters as facebook } from '../adapters/facebook';
+import { adapters as google } from '../adapters/google';
+import { adapters as yandex } from '../adapters/yandex';
 
-const __dirname = getDirname();
-const adapterDir = join(__dirname, '..', 'adapters');
-
-export const getAllAdapters = async () =>
-    Promise.all(
-        (await readdir(adapterDir))
-            .map((f) => join(adapterDir, f))
-            .map((f) =>
-                import(f).then((i) => {
-                    const adapters = i.adapters as Adapter[];
-                    if (adapters.some((a) => a.tracker.slug !== basename(f, '.ts')))
-                        throw new Error("Tracker slug in adapter doesn't match filename.");
-                    return adapters;
-                })
-            )
-    ).then((a) => a.flat());
+export const allAdapters = [...facebook, ...google, ...yandex];
