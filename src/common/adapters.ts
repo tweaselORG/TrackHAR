fd3c31b07b2db897f59c24357e53b5481582be67
Benjamin Altpeter <hi@bn.al>
2023-04-06 12:21:04 +0200

    Implement changes from #4

--- src/common/adapters.ts
+++ src/common/adapters.ts
@@ -1,5 +1,39 @@
+import { adapters as adcolony } from '../adapters/adcolony';
+import { adapters as adjust } from '../adapters/adjust';
+import { adapters as apple } from '../adapters/apple';
+import { adapters as branchIo } from '../adapters/branch-io';
+import { adapters as chartboost } from '../adapters/chartboost';
 import { adapters as facebook } from '../adapters/facebook';
 import { adapters as google } from '../adapters/google';
+import { adapters as infonline } from '../adapters/infonline';
+import { adapters as ironsource } from '../adapters/ironsource';
+import { adapters as microsoft } from '../adapters/microsoft';
+import { adapters as mopub } from '../adapters/mopub';
+import { adapters as onesignal } from '../adapters/onesignal';
+import { adapters as rayjump } from '../adapters/rayjump';
+import { adapters as smartbear } from '../adapters/smartbear';
+import { adapters as startIo } from '../adapters/start-io';
+import { adapters as unity } from '../adapters/unity';
+import { adapters as vungle } from '../adapters/vungle';
 import { adapters as yandex } from '../adapters/yandex';
 
-export const allAdapters = [...facebook, ...google, ...yandex];
+export const allAdapters = [
+    ...adcolony,
+    ...adjust,
+    ...apple,
+    ...branchIo,
+    ...chartboost,
+    ...facebook,
+    ...google,
+    ...infonline,
+    ...ironsource,
+    ...microsoft,
+    ...mopub,
+    ...onesignal,
+    ...rayjump,
+    ...smartbear,
+    ...startIo,
+    ...unity,
+    ...vungle,
+    ...yandex,
+];
