81c863eb0225e147e2a0862b8d244dc27153316c
Benjamin Altpeter <hi@bn.al>
2023-04-13 13:28:12 +0200

    `facebook`: Untangle `adnw-sync2` and `graph-network-ads-common`

--- src/adapters/facebook.ts
+++ src/adapters/facebook.ts
@@ -59,342 +59,6 @@ const graphContainedDataPaths: Adapter['containedDataPaths'] = {
         reasoning: 'obvious property name',
     },
 };
-const graph2ContainedDataPaths: Adapter['containedDataPaths'] = {
-    appId: [
-        {
-            context: 'body',
-            path: 'BUNDLE',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.BUNDLE',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    appVersion: [
-        {
-            context: 'body',
-            path: 'APPVERS',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.APPVERS',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    idfa: [
-        {
-            context: 'body',
-            path: 'IDFA',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.IDFA',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    otherIdentifiers: [
-        {
-            context: 'body',
-            path: 'SESSION_ID',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'ANON_ID',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.SESSION_ID',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ANON_ID',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    manufacturer: [
-        {
-            context: 'body',
-            path: 'MAKE',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.MAKE',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    model: [
-        {
-            context: 'body',
-            path: 'MODEL',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.MODEL',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    isEmulator: [
-        {
-            context: 'body',
-            path: 'VALPARAMS.is_emu',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.VALPARAMS.is_emu',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    isRooted: [
-        {
-            context: 'body',
-            path: 'ROOTED',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ROOTED',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    carrier: [
-        {
-            context: 'body',
-            path: 'CARRIER',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.CARRIER',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    screenHeight: [
-        {
-            context: 'body',
-            path: 'SCREEN_HEIGHT',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.SCREEN_HEIGHT',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    screenWidth: [
-        {
-            context: 'body',
-            path: 'SCREEN_WIDTH',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.SCREEN_WIDTH',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    osName: [
-        {
-            context: 'body',
-            path: 'OS',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.OS',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    osVersion: [
-        {
-            context: 'body',
-            path: 'OSVERS',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.OSVERS',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    isCharging: [
-        {
-            context: 'body',
-            path: 'ANALOG.charging',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ANALOG.charging',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    batteryLevel: [
-        {
-            context: 'body',
-            path: 'ANALOG.battery',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ANALOG.battery',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    ramTotal: [
-        {
-            context: 'body',
-            path: 'ANALOG.total_memory',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ANALOG.total_memory',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    ramFree: [
-        {
-            context: 'body',
-            path: 'ANALOG.available_memory',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ANALOG.available_memory',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    diskFree: [
-        {
-            context: 'body',
-            path: 'ANALOG.free_space',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ANALOG.free_space',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    accelerometerX: [
-        {
-            context: 'body',
-            path: 'ANALOG.accelerometer_x',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ANALOG.accelerometer_x',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    accelerometerY: [
-        {
-            context: 'body',
-            path: 'ANALOG.accelerometer_y',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ANALOG.accelerometer_y',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    accelerometerZ: [
-        {
-            context: 'body',
-            path: 'ANALOG.accelerometer_z',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ANALOG.accelerometer_z',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    rotationX: [
-        {
-            context: 'body',
-            path: 'ANALOG.rotation_x',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ANALOG.rotation_x',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    rotationY: [
-        {
-            context: 'body',
-            path: 'ANALOG.rotation_y',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ANALOG.rotation_y',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    rotationZ: [
-        {
-            context: 'body',
-            path: 'ANALOG.rotation_z',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.ANALOG.rotation_z',
-            reasoning: 'obvious property name',
-        },
-    ],
-
-    language: [
-        {
-            context: 'body',
-            path: 'LOCALE',
-            reasoning: 'obvious property name',
-        },
-        {
-            context: 'body',
-            path: 'context.LOCALE',
-            reasoning: 'obvious property name',
-        },
-    ],
-};
 
 export const adapters: Adapter[] = [
     {
@@ -443,7 +107,85 @@ export const adapters: Adapter[] = [
         endpointUrls: ['https://graph.facebook.com/network_ads_common'],
 
         decodingSteps: [{ function: 'parseQueryString', input: 'body', output: 'res.body' }],
-        containedDataPaths: graph2ContainedDataPaths,
+        containedDataPaths: {
+            appId: {
+                context: 'body',
+                path: 'BUNDLE',
+                reasoning: 'obvious property name',
+            },
+
+            appVersion: {
+                context: 'body',
+                path: 'APPVERS',
+                reasoning: 'obvious property name',
+            },
+
+            idfa: {
+                context: 'body',
+                path: 'IDFA',
+                reasoning: 'obvious property name',
+            },
+
+            otherIdentifiers: {
+                context: 'body',
+                path: 'SESSION_ID',
+                reasoning: 'obvious property name',
+            },
+
+            manufacturer: {
+                context: 'body',
+                path: 'MAKE',
+                reasoning: 'obvious property name',
+            },
+
+            model: {
+                context: 'body',
+                path: 'MODEL',
+                reasoning: 'obvious property name',
+            },
+
+            isRooted: {
+                context: 'body',
+                path: 'ROOTED',
+                reasoning: 'obvious property name',
+            },
+
+            carrier: {
+                context: 'body',
+                path: 'CARRIER',
+                reasoning: 'obvious property name',
+            },
+
+            screenHeight: {
+                context: 'body',
+                path: 'SCREEN_HEIGHT',
+                reasoning: 'obvious property name',
+            },
+
+            screenWidth: {
+                context: 'body',
+                path: 'SCREEN_WIDTH',
+                reasoning: 'obvious property name',
+            },
+
+            osName: {
+                context: 'body',
+                path: 'OS',
+                reasoning: 'obvious property name',
+            },
+
+            osVersion: {
+                context: 'body',
+                path: 'OSVERS',
+                reasoning: 'obvious property name',
+            },
+
+            language: {
+                context: 'body',
+                path: 'LOCALE',
+                reasoning: 'obvious property name',
+            },
+        },
     },
 
     {
@@ -456,9 +198,159 @@ export const adapters: Adapter[] = [
             { function: 'parseQueryString', input: 'body', output: 'q' },
             { function: 'getProperty', input: 'q', options: { path: 'payload' }, output: 'payload' },
             { function: 'parseJson', input: 'payload', output: 'res.body' },
-            { function: 'parseJson', input: 'res.body.VALPARAMS', output: 'res.body.VALPARAMS' },
-            { function: 'parseJson', input: 'res.body.ANALOG', output: 'res.body.ANALOG' },
+            { function: 'parseJson', input: 'res.body.context.VALPARAMS', output: 'res.body.context.VALPARAMS' },
+            { function: 'parseJson', input: 'res.body.context.ANALOG', output: 'res.body.context.ANALOG' },
         ],
-        containedDataPaths: graph2ContainedDataPaths,
+        containedDataPaths: {
+            appId: {
+                context: 'body',
+                path: 'context.BUNDLE',
+                reasoning: 'obvious property name',
+            },
+
+            appVersion: {
+                context: 'body',
+                path: 'context.APPVERS',
+                reasoning: 'obvious property name',
+            },
+
+            idfa: {
+                context: 'body',
+                path: 'context.IDFA',
+                reasoning: 'obvious property name',
+            },
+
+            otherIdentifiers: {
+                context: 'body',
+                path: 'context.SESSION_ID',
+                reasoning: 'obvious property name',
+            },
+
+            manufacturer: {
+                context: 'body',
+                path: 'context.MAKE',
+                reasoning: 'obvious property name',
+            },
+
+            model: {
+                context: 'body',
+                path: 'context.MODEL',
+                reasoning: 'obvious property name',
+            },
+
+            isEmulator: {
+                context: 'body',
+                path: 'context.VALPARAMS.is_emu',
+                reasoning: 'obvious property name',
+            },
+
+            isRooted: {
+                context: 'body',
+                path: 'context.ROOTED',
+                reasoning: 'obvious property name',
+            },
+
+            carrier: {
+                context: 'body',
+                path: 'context.CARRIER',
+                reasoning: 'obvious property name',
+            },
+
+            screenHeight: {
+                context: 'body',
+                path: 'context.SCREEN_HEIGHT',
+                reasoning: 'obvious property name',
+            },
+
+            screenWidth: {
+                context: 'body',
+                path: 'context.SCREEN_WIDTH',
+                reasoning: 'obvious property name',
+            },
+
+            osName: {
+                context: 'body',
+                path: 'context.OS',
+                reasoning: 'obvious property name',
+            },
+
+            osVersion: {
+                context: 'body',
+                path: 'context.OSVERS',
+                reasoning: 'obvious property name',
+            },
+
+            isCharging: {
+                context: 'body',
+                path: 'context.ANALOG.charging',
+                reasoning: 'obvious property name',
+            },
+
+            batteryLevel: {
+                context: 'body',
+                path: 'context.ANALOG.battery',
+                reasoning: 'obvious property name',
+            },
+
+            ramTotal: {
+                context: 'body',
+                path: 'context.ANALOG.total_memory',
+                reasoning: 'obvious property name',
+            },
+
+            ramFree: {
+                context: 'body',
+                path: 'context.ANALOG.available_memory',
+                reasoning: 'obvious property name',
+            },
+
+            diskFree: {
+                context: 'body',
+                path: 'context.ANALOG.free_space',
+                reasoning: 'obvious property name',
+            },
+
+            accelerometerX: {
+                context: 'body',
+                path: 'context.ANALOG.accelerometer_x',
+                reasoning: 'obvious property name',
+            },
+
+            accelerometerY: {
+                context: 'body',
+                path: 'context.ANALOG.accelerometer_y',
+                reasoning: 'obvious property name',
+            },
+
+            accelerometerZ: {
+                context: 'body',
+                path: 'context.ANALOG.accelerometer_z',
+                reasoning: 'obvious property name',
+            },
+
+            rotationX: {
+                context: 'body',
+                path: 'context.ANALOG.rotation_x',
+                reasoning: 'obvious property name',
+            },
+
+            rotationY: {
+                context: 'body',
+                path: 'context.ANALOG.rotation_y',
+                reasoning: 'obvious property name',
+            },
+
+            rotationZ: {
+                context: 'body',
+                path: 'context.ANALOG.rotation_z',
+                reasoning: 'obvious property name',
+            },
+
+            language: {
+                context: 'body',
+                path: 'context.LOCALE',
+                reasoning: 'obvious property name',
+            },
+        },
     },
 ];
