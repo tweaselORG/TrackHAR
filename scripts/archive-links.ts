6bbc8abc386a328186c8e5fdc3b63e1c85cfd684
Lorenz Sieben <me@lorenz-sieben.com>
2023-09-27 16:30:08 +0200

    Scope urls to properties

--- scripts/archive-links.ts
+++ scripts/archive-links.ts
@@ -4,9 +4,15 @@ import chalk from 'chalk';
 // eslint-disable-next-line import/no-unresolved
 import { parse, stringify } from 'csv/sync';
 import { readFile, writeFile } from 'fs/promises';
+import pMap, { pMapSkip } from 'p-map';
 import { adapters } from '../src/index';
 import type { ArchiveOrgAuth, CaptureFailedException } from './lib/wayback';
-import { captureAndWait, isCaptureFailedException, isCapturePendingException } from './lib/wayback';
+import {
+    archiveOrgTimestampToDate,
+    captureAndWait,
+    isCaptureFailedException,
+    isCapturePendingException,
+} from './lib/wayback';
 
 /** These errors thrown by the Wayback Machine API are considered temporary and will be retried. */
 const errorsToRetry = [
@@ -30,6 +36,13 @@ const csvPath = './research-docs/archived-urls.csv';
 const errorPath = './research-docs/archive-errors.json';
 const configPath = './archive-config.json';
 
+type ArchivedUrlEntry = {
+    originalUrl: string;
+    archivedUrl: string;
+    timestamp: Date;
+    referringDataPaths: Set<string>;
+};
+
 (async () => {
     const config = await readFile(configPath, 'utf-8').then(JSON.parse);
     if (!config.waybackAuth)
@@ -41,9 +54,23 @@ const configPath = './archive-config.json';
     const linkRegex = /^https?:\/\//;
 
     const urlFile = await readFile(csvPath, 'utf-8');
-    const urlCSV = parse(urlFile, { bom: true, delimiter: ',' });
-    const archivedURLs = urlCSV.reduce((acc: Record<string, string>, cur: string[2]) => {
-        acc[cur[0]] = cur[1];
+    const urlCsv = parse(urlFile, {
+        bom: true,
+        delimiter: ',',
+        columns: true,
+        cast: (value, context) => {
+            if (context.column === 'referringDataPaths') return new Set(value.split(';'));
+            else if (context.column === 'timestamp') return new Date(value);
+            return value;
+        },
+    }) as ArchivedUrlEntry[];
+
+    const archivedURLs = new Set<string>();
+    const archivedDataPaths = urlCsv.reduce((acc: Record<string, Set<string>>, cur) => {
+        console.log(cur);
+        archivedURLs.add(cur.originalUrl);
+        // We save the data paths as a Set to avoid duplicates (e.g. if a URL has been archived twice for a data path) and because of the performance benefit when we check if a data path is already archived.
+        acc[cur.originalUrl] = new Set([...(acc[cur.originalUrl] || []), ...cur.referringDataPaths]);
         return acc;
     }, {});
 
@@ -79,16 +106,22 @@ const configPath = './archive-config.json';
         string,
         { try: number; lastTried: number; error: ReturnType<CaptureFailedException['toJSON']> }
     >;
-    const newURLs: [string, string][] = [];
 
-    await Promise.all(
-        Object.entries(reasoningURLs).map(async ([url, dataPaths]) => {
-            if (Object.keys(archivedURLs).includes(url)) return;
+    const newURLs = await pMap(
+        Object.entries(reasoningURLs),
+        async ([url, dataPaths]) => {
+            const unarchivedPaths = archivedURLs.has(url)
+                ? dataPaths.filter((path) => !archivedDataPaths[url].has(path))
+                : dataPaths;
+
+            console.log(unarchivedPaths);
+
+            if (unarchivedPaths.length < 1) return pMapSkip;
             else if (Object.keys(archiveErrors).includes(url)) {
                 // Do an exponential backoff for retries and retry after 15, 30, 60, 120 and 240 minutes
                 const whenToTryNext = archiveErrors[url].lastTried + 2 ** archiveErrors[url].try * 750000;
                 if (whenToTryNext > new Date().getTime() && !errorsToRetry.includes(archiveErrors[url].error.type))
-                    return;
+                    return pMapSkip;
             }
 
             try {
@@ -96,13 +129,19 @@ const configPath = './archive-config.json';
                 const savePageResult = await captureAndWait(url, waybackAuth, {
                     skipFirstArchive: true,
                 });
-                newURLs.push([url, savePageResult.captureUrl]);
+
                 delete archiveErrors[url];
+                return {
+                    originalUrl: url,
+                    archivedUrl: savePageResult.captureUrl,
+                    timestamp: archiveOrgTimestampToDate(savePageResult.captureStatus.timestamp),
+                    referringDataPaths: new Set(unarchivedPaths),
+                };
             } catch (e) {
                 if (isCaptureFailedException(e)) {
                     console.warn(
                         chalk.bold.red(`Error while archiving "${url}": \n ${e.message}`) +
-                            chalk.red(`\n -> Affected data paths: ${dataPaths.join(', ')}`)
+                            chalk.red(`\n -> Affected data paths: ${unarchivedPaths.join(', ')}`)
                     );
                     archiveErrors[url] = {
                         try: archiveErrors[url]?.try + 1 || 1,
@@ -112,14 +151,31 @@ const configPath = './archive-config.json';
                 } else if (isCapturePendingException(e)) {
                     console.warn(
                         chalk.bold.yellow(`Timed out while waiting for archiving status of "${url}".`) +
-                            chalk.yellow(`\n -> Affected data paths: ${dataPaths.join(', ')}`)
+                            chalk.yellow(`\n -> Affected data paths: ${unarchivedPaths.join(', ')}`)
                     );
                 } else throw e;
             }
-        })
+
+            // Exclude empty results
+            return pMapSkip;
+        },
+        { concurrency: 5 }
     );
 
-    if (newURLs.length > 0) await writeFile(csvPath, stringify([...urlCSV, ...newURLs], { quoted: true }));
+    if (newURLs.length > 0) {
+        await writeFile(
+            csvPath,
+            stringify([...urlCsv, ...newURLs], {
+                quoted: true,
+                header: true,
+                columns: ['originalUrl', 'archivedUrl', 'timestamp', 'referringDataPaths'],
+                cast: {
+                    object: (value) => (value instanceof Set ? [...value].join(';') : JSON.stringify(value)),
+                    date: (value) => value.toISOString(),
+                },
+            })
+        );
+    }
     await writeFile(errorPath, JSON.stringify(archiveErrors, null, 4));
 })();
 /* eslint-enable no-console */
