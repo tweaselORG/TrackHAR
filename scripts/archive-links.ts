aac4f4762ba454b4361bf0e3bff46ea90b731bf1
Lorenz Sieben <me@lorenz-sieben.com>
2023-09-28 14:56:09 +0200

    Queue file changes in the auto-archiver
    This was surprisingly hard to do. Because nodejs caches imports, we need to somehow clear the import cache.
    This can be done naively (https://ar.al/2021/02/22/cache-busting-in-node.js-dynamic-esm-imports/#cache-invalidation-in-esm-with-dynamic-imports) for plain js files, but it leaks memory and fails for typescript files, since the lodaer doesn’t support it.
    So, we run each archiving task in a new worker thread that has its own import context. But, since the adapters we are importing are written in typescript, we need to register a loader in the worker context, because it doesn’t have it loaded by default (like the main thread does via `tsx`).
    Luckly, this feature has just been added to node, but we need to bump our node version to use it. Now we can dynamically import the all chnages to modules.

--- scripts/archive-links.ts
+++ scripts/archive-links.ts
@@ -1,11 +1,19 @@
 /* eslint-disable no-console */
+if (process.version < 'v20') throw new Error('This script requires Node.js v20 or higher.');
 
 import chalk from 'chalk';
+import { watch } from 'chokidar';
+// eslint-plugin-import doesn't support the "exports" field in package.json, yet (https://github.com/import-js/eslint-plugin-import/issues/1810).
 // eslint-disable-next-line import/no-unresolved
 import { parse, stringify } from 'csv/sync';
 import { readFile, writeFile } from 'fs/promises';
+import { register } from 'module';
 import pMap, { pMapSkip } from 'p-map';
-import { adapters } from '../src/index';
+// eslint-disable-next-line import/no-unresolved
+import PQueue from 'p-queue';
+import { resolve } from 'path';
+import { isMainThread, parentPort, Worker, workerData } from 'worker_threads';
+import type { Adapter } from '../src/index';
 import type { ArchiveOrgAuth, CaptureFailedException } from './lib/wayback';
 import {
     archiveOrgTimestampToDate,
@@ -43,14 +51,7 @@ type ArchivedUrlEntry = {
     referringDataPaths: Set<string>;
 };
 
-(async () => {
-    const config = await readFile(configPath, 'utf-8').then(JSON.parse);
-    if (!config.waybackAuth)
-        throw new Error(
-            'Please provide your archive.org credentials in archive-config.json. You can find them at https://archive.org/account/s3.php.'
-        );
-    const waybackAuth = config.waybackAuth as ArchiveOrgAuth;
-
+const archiveUrls = async (waybackAuth: ArchiveOrgAuth, adapters: Adapter[]) => {
     const linkRegex = /^https?:\/\//;
 
     const urlFile = await readFile(csvPath, 'utf-8');
@@ -67,7 +68,6 @@ type ArchivedUrlEntry = {
 
     const archivedURLs = new Set<string>();
     const archivedDataPaths = urlCsv.reduce((acc: Record<string, Set<string>>, cur) => {
-        console.log(cur);
         archivedURLs.add(cur.originalUrl);
         // We save the data paths as a Set to avoid duplicates (e.g. if a URL has been archived twice for a data path) and because of the performance benefit when we check if a data path is already archived.
         acc[cur.originalUrl] = new Set([...(acc[cur.originalUrl] || []), ...cur.referringDataPaths]);
@@ -114,8 +114,6 @@ type ArchivedUrlEntry = {
                 ? dataPaths.filter((path) => !archivedDataPaths[url].has(path))
                 : dataPaths;
 
-            console.log(unarchivedPaths);
-
             if (unarchivedPaths.length < 1) return pMapSkip;
             else if (Object.keys(archiveErrors).includes(url)) {
                 // Do an exponential backoff for retries and retry after 15, 30, 60, 120 and 240 minutes
@@ -125,12 +123,13 @@ type ArchivedUrlEntry = {
             }
 
             try {
-                console.info(`Archiving ${url}...`);
                 const savePageResult = await captureAndWait(url, waybackAuth, {
                     skipFirstArchive: true,
                 });
 
                 delete archiveErrors[url];
+                console.info(chalk.green(`Archived "${url}" for ${unarchivedPaths.join(', ')}.`));
+
                 return {
                     originalUrl: url,
                     archivedUrl: savePageResult.captureUrl,
@@ -139,7 +138,7 @@ type ArchivedUrlEntry = {
                 };
             } catch (e) {
                 if (isCaptureFailedException(e)) {
-                    console.warn(
+                    console.error(
                         chalk.bold.red(`Error while archiving "${url}": \n ${e.message}`) +
                             chalk.red(`\n -> Affected data paths: ${unarchivedPaths.join(', ')}`)
                     );
@@ -159,7 +158,7 @@ type ArchivedUrlEntry = {
             // Exclude empty results
             return pMapSkip;
         },
-        { concurrency: 5 }
+        { concurrency: 3 }
     );
 
     if (newURLs.length > 0) {
@@ -177,5 +176,61 @@ type ArchivedUrlEntry = {
         );
     }
     await writeFile(errorPath, JSON.stringify(archiveErrors, null, 4));
-})();
+};
+
+const workerTask = async (workerData: { waybackAuth: ArchiveOrgAuth; path: string }) => {
+    register('tsx/esm', import.meta.url);
+    try {
+        // The import might fail because the syntax is invalid. We don't want to crash the whole process because of that.
+        const adapterModule = await import(`${resolve('.', workerData.path)}?version=${new Date().getTime()}`);
+        if (adapterModule.adapters) await archiveUrls(workerData.waybackAuth, adapterModule.adapters);
+        parentPort?.postMessage('archive-worker:success');
+    } catch (e) {
+        console.error(
+            chalk.bold.red('Error while parsing adapter at ') + chalk.red(workerData.path) + chalk.bold.red(': \n'),
+            chalk.red(e.toString())
+        );
+        parentPort?.postMessage('archive-worker:parse-error');
+    }
+};
+
+if (isMainThread)
+    (async () => {
+        const config = await readFile(configPath, 'utf-8').then(JSON.parse);
+        if (!config.waybackAuth)
+            throw new Error(
+                'Please provide your archive.org credentials in archive-config.json. You can find them at https://archive.org/account/s3.php.'
+            );
+        const waybackAuth = config.waybackAuth as ArchiveOrgAuth;
+
+        const archiveQueue = new PQueue({ concurrency: 1 });
+
+        // TODO: Remove entries for the same file that have not run, yet.
+        const onAdapterChange = (path: string) =>
+            archiveQueue.add(
+                () =>
+                    new Promise<void>((resolve, reject) => {
+                        const worker = new Worker(new URL(import.meta.url), {
+                            workerData: { path, waybackAuth },
+                        });
+
+                        worker.on('message', (result) => {
+                            if (result === 'archive-worker:success') resolve();
+                            else if (result === 'archive-worker:parse-error') resolve();
+                        });
+
+                        worker.on('error', reject);
+                        worker.on('exit', (code) => {
+                            if (code !== 0) {
+                                reject(new Error(`Worker stopped with exit code ${code}`));
+                            }
+                        });
+                    })
+            );
+
+        const watcher = watch('src/adapters/**/*.ts', { awaitWriteFinish: true });
+        watcher.on('add', onAdapterChange);
+        watcher.on('change', onAdapterChange);
+    })();
+else workerTask(workerData);
 /* eslint-enable no-console */
